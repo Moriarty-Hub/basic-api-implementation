2ec5b373c0f144625e9a446c3ee7d2bbc4193014
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-04 18:30:02 +0800

    Refactor: add a new data type RsEvent for events.

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,23 +1,31 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.bean.RsEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private final List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private final List<RsEvent> rsList = new LinkedList<>();
+
+  public RsController() {
+    rsList.add(new RsEvent("美股熔断", "经济"));
+    rsList.add(new RsEvent("边境冲突", "军事"));
+    rsList.add(new RsEvent("示威活动", "自由"));
+  }
 
   @GetMapping("/rs/getEvent")
-  public String getEvent(@RequestParam int id) {
+  public RsEvent getEvent(@RequestParam int id) {
     return rsList.get(id - 1);
   }
 
   @GetMapping("/rs/getEventList")
-  public List<String> getEventListOfSpecifiedRange(@RequestParam int start, @RequestParam int end) {
+  public List<RsEvent> getEventListOfSpecifiedRange(@RequestParam int start, @RequestParam int end) {
     return rsList.subList(start - 1, end);
   }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -21,13 +21,16 @@ class RsListApplicationTests {
     @Test
     void should_return_rs_event_of_specified_index() throws Exception {
         mockMvc.perform(get("/rs/getEvent?id=1"))
-                .andExpect(content().string("第一条事件"))
+                .andExpect(jsonPath("$.name", is("美股熔断")))
+                .andExpect(jsonPath("$.keyword", is("经济")))
                 .andExpect(status().isOk());
         mockMvc.perform(get("/rs/getEvent?id=2"))
-                .andExpect(content().string("第二条事件"))
+                .andExpect(jsonPath("$.name", is("边境冲突")))
+                .andExpect(jsonPath("$.keyword", is("军事")))
                 .andExpect(status().isOk());
         mockMvc.perform(get("/rs/getEvent?id=3"))
-                .andExpect(content().string("第三条事件"))
+                .andExpect(jsonPath("$.name", is("示威活动")))
+                .andExpect(jsonPath("$.keyword", is("自由")))
                 .andExpect(status().isOk());
     }
 
@@ -35,19 +38,26 @@ class RsListApplicationTests {
     void should_return_rs_event_list_between_specified_range() throws Exception {
         mockMvc.perform(get("/rs/getEventList?start=1&end=3"))
                 .andExpect(jsonPath("$", hasSize(3)))
-                .andExpect(jsonPath("$[0]", is("第一条事件")))
-                .andExpect(jsonPath("$[1]", is("第二条事件")))
-                .andExpect(jsonPath("$[2]", is("第三条事件")))
+                .andExpect(jsonPath("$[0].name", is("美股熔断")))
+                .andExpect(jsonPath("$[0].keyword", is("经济")))
+                .andExpect(jsonPath("$[1].name", is("边境冲突")))
+                .andExpect(jsonPath("$[1].keyword", is("军事")))
+                .andExpect(jsonPath("$[2].name", is("示威活动")))
+                .andExpect(jsonPath("$[2].keyword", is("自由")))
                 .andExpect(status().isOk());
         mockMvc.perform(get("/rs/getEventList?start=1&end=2"))
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0]", is("第一条事件")))
-                .andExpect(jsonPath("$[1]", is("第二条事件")))
+                .andExpect(jsonPath("$[0].name", is("美股熔断")))
+                .andExpect(jsonPath("$[0].keyword", is("经济")))
+                .andExpect(jsonPath("$[1].name", is("边境冲突")))
+                .andExpect(jsonPath("$[1].keyword", is("军事")))
                 .andExpect(status().isOk());
         mockMvc.perform(get("/rs/getEventList?start=2&end=3"))
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0]", is("第二条事件")))
-                .andExpect(jsonPath("$[1]", is("第三条事件")))
+                .andExpect(jsonPath("$[0].name", is("边境冲突")))
+                .andExpect(jsonPath("$[0].keyword", is("军事")))
+                .andExpect(jsonPath("$[1].name", is("示威活动")))
+                .andExpect(jsonPath("$[1].keyword", is("自由")))
                 .andExpect(status().isOk());
     }
 
